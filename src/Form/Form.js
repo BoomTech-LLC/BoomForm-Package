8b5a1b5cf253742bed6a99864e63f5c62f1f457d
paployanjr <tigranp@gmail.com>
2021-08-10 11:49:26 +0400

    creating onSubmit function

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import context from '../Store/Context'
 import Header from './Header'
 import Footer from './Footer'
 import Fields from './Fields'
 
 const Form = ({ global, fields }) => {
-  const { name, description, button } = global
+  const { state, actions } = useContext(context)
+  const { name, description, button, onSubmit } = global
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (onSubmit) onSubmit({ state, actions })
+  }
 
   return (
-    <form className='boomForm' noValidate>
+    <form className='boomForm' onSubmit={handleSubmit} noValidate>
       <Header name={name} description={description} />
       <Fields fields={fields} />
       <Footer button={button} />
