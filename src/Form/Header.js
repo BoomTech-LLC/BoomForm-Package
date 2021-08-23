f4c053895d31af21b3b670bb65730f1e7b29dc10
Tigran <tikonazaryan5@gmail.com>
2021-08-23 16:32:36 +0400

    adding classname to header

--- src/Form/Header.js
+++ src/Form/Header.js
@@ -3,7 +3,7 @@ import Pagination from './Pagination/Header/Pagination'
 
 const Header = ({ name, description, ...props }) => {
   return (
-    <div>
+    <div className='boomForm__header'>
       <div className='boomForm__name'>{name}</div>
       <div className='boomForm__description'>{description}</div>
       <Pagination {...props} />
