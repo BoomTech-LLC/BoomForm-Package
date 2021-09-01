f4d4d6db694802e7cfee0902e28fcc778a200797
Tigran <tikonazaryan5@gmail.com>
2021-09-01 12:20:04 +0400

    Removing Couple Useless Functions

--- src/Helpers/global.js
+++ src/Helpers/global.js
@@ -80,16 +80,6 @@ export const deepCopy = (object) => {
   return newObject
 }
 
-export const getPrintableFields = (fields, logic = [], pagination = []) => {
-  const printableFields = fields.flatMap(({ id }) =>
-    !logic.includes(id) ? id : []
-  )
-  return [printableFields, pagination].reduce((a, c) => {
-    if (!c.length) return a
-    return a.filter((i) => c.includes(i))
-  })
-}
-
 export const getIdsByName = (name, fields) => {
   const ids = []
   fields.map((field) => {
