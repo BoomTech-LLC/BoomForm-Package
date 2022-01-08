d1e79d09b9d715f785b53442c74510c5dfa90cfa
Tigran <tikonazaryan5@gmail.com>
2022-01-08 22:22:32 +0400

    Email Validation Changes

--- src/Helpers/validate.js
+++ src/Helpers/validate.js
@@ -85,7 +85,7 @@ export const validate = ({ value, validation, type }) => {
         break
 
       case 'email':
-        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) && value)
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.{1,15}$/i.test(value) && value)
           return msg
         break
 
