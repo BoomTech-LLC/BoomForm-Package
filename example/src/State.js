24c20d482e7d0a0646d65cd3f3a1e7183293986f
Amram1997 <amramananyan9@gmail.com>
2022-09-10 21:45:27 +0400

    add new values for validate function

--- example/src/State.js
+++ example/src/State.js
@@ -1,9 +1,16 @@
 import React, { useContext } from 'react'
 import { Context, useField } from 'boomform'
 
-const State = ({ actions }) => {
-  const x = useField(['a', 'b'])
-  console.log('x', x)
+// const State = ({ actions }) => {
+//   const x = useField(['a', 'b'])
+//   console.log('x', x)
+//   return null
+// }
+const State = () => {
+  const store= useContext(Context)
+ 
+  console.log(store)
+ 
   return null
 }
 
