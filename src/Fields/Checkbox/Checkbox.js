4249fa8913a123d6dab5e37968e3d5f2ca9da55f
Amram1997 <amramananyan9@gmail.com>
2022-12-02 12:33:55 +0400

    change handle change function

--- src/Fields/Checkbox/Checkbox.js
+++ src/Fields/Checkbox/Checkbox.js
@@ -34,7 +34,7 @@ const Checkbox = ({ id, initial, validation = {}, ...props }) => {
     if (HTMLValidate === true)
       handleValidationChange({ e, possibleError: errors[id] })
 
-    handleChange({ id, value: e.target.checked })
+    handleChange({ id, value: e.target.checked, e, ref, })
   }
 
   const onBlur = (e) => {
