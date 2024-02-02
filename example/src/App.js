7da79b3c27e4fba87cf2f53da4228456e28e47ec
Tigran0199 <cruelman11@gmail.com>
2024-02-02 20:08:08 +0400

    fix: optimized useField hook

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BoomForm, Input, useField } from 'boomform'
 
-const Barev = ({ x }) => {
-  const { neededValues } = useField(['qwe'])
-
-  console.log('neededValues', neededValues)
-
-  return <div>{x}</div>
-}
-
 const Hajox = () => {
-  const [x, setX] = useState(0)
+  const fieldsData = useField(['2', '3', '4', '5', '6', '7'])
 
+  console.log('====================================')
+  console.log('USEFIELD RETURN ', fieldsData)
+  console.log('====================================')
   return (
     <>
-      <div onClick={() => setX(x + 1)}>Barev</div>
-      <Barev x={x} />
-      <Input id='qwe' placeholder='Barev' />
+      <Input id='2' initial='Barev2' placeholder='Barev2' />
+      <Input id='3' placeholder='Barev3' />
+      <Input id='4' placeholder='Barev4' />
+      <Input id='5' placeholder='Barev5' />
+      <Input id='6' placeholder='Barev6' />
+      <Input id='7' placeholder='Barev7' />
     </>
   )
 }
@@ -24,7 +22,7 @@ const Hajox = () => {
 const App = () => {
   return (
     <BoomForm>
-      {({ updateId }) => (
+      {() => (
         <>
           <Hajox />
         </>
--- src/Hooks/useField.js
+++ src/Hooks/useField.js
@@ -1,10 +1,73 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import Emitter from './../Events'
-import {
-  getFieldValue,
-  setNestedValue,
-  getUseFieldInitial
-} from './../Helpers/global'
+// import {
+//   getFieldValue,
+//   setNestedValue,
+//   getUseFieldInitial
+// } from './../Helpers/global'
+
+const isObject = (variable) => {
+  return (
+    typeof variable === 'object' &&
+    !Array.isArray(variable) &&
+    variable !== null
+  )
+}
+const getFieldValue = (values, id) => {
+  if (!id) throw 'field should have a id attribute'
+  if (!id.toString().includes('.')) return values[id]
+  let idPath = id.split('.')
+  idPath = idPath.filter((path) => path)
+  return [values].concat(idPath).reduce((acc, val) => {
+    if (!acc) return undefined
+    return acc[val]
+  })
+}
+const setNestedValue = (key, value, values) => {
+  const keyPath = key.toString().split('.')
+  keyPath.reduce((acc, val, index) => {
+    if (keyPath.length - 1 === index) {
+      return (acc[val] = value)
+    }
+    if (!isObject(acc[val]) || acc[val] === null) return (acc[val] = {})
+    else return acc[val]
+  }, values)
+
+  // console.log('Setting nested value', values)
+  return values
+}
+
+const getUseFieldInitial = (ids) => {
+  const structuredData = {
+    id: null,
+    value: null,
+    touched: null,
+    ids,
+    neededValues: {},
+    prevState: {},
+    newErrors: [],
+    newTouched: []
+  }
+
+  if (window.__current_form_state) {
+    const { values, touched, errors } = window.__current_form_state
+    let neededValues = {}
+
+    for (let i = 0; i < ids.length; i++)
+      neededValues = setNestedValue(
+        ids[i],
+        getFieldValue(values, ids[i]),
+        neededValues
+      )
+
+    structuredData.neededValues = neededValues
+    structuredData.prevState = window.__current_form_state
+    structuredData.newErrors = errors
+    structuredData.newTouched = touched
+  }
+  // console.log('Returning', structuredData)
+  return structuredData
+}
 
 const useField = (ids) => {
   const [data, setData] = useState({
@@ -18,48 +81,48 @@ const useField = (ids) => {
     newValues: {},
     newTouched: {}
   })
-
+  const initialValues = useMemo(() => {
+    return getUseFieldInitial(ids)
+  }, [
+    JSON.stringify(ids),
+    JSON.stringify(Object.keys(window.__current_form_state || []))
+  ])
   const current_event = useRef(null)
 
-  const handleDataSet = (payload) => {
-    const { state, errors, values, touched, id } = payload
- 
-    let neededValues = {}
-
-    for (let i = 0; i < ids.length; i++)
-      neededValues = setNestedValue(
-        ids[i],
-        getFieldValue(values, ids[i]),
-        neededValues
-      )
-
-    const structuredData = {
-      id,
-      value: getFieldValue(values, id),
-      touched: touched[id],
-      ids,
-      neededValues,
-      prevState: state,
-      newErrors: errors,
-      newValues: values,
-      newTouched: touched
-    }
-
-    setData(structuredData)
-  }
+  const handleDataSet = useCallback(
+    (payload) => {
+      const { state, errors, values, touched, id } = payload
+      const neededValues = ids.reduce((acc, id) => {
+        return setNestedValue(id, getFieldValue(values, id), acc)
+      }, {})
+      const structuredData = {
+        id,
+        value: getFieldValue(values, id),
+        touched: touched[id],
+        ids,
+        neededValues,
+        prevState: state,
+        newErrors: errors,
+        newValues: values,
+        newTouched: touched
+      }
+      setData(structuredData)
+    },
+    [ids]
+  )
 
   useEffect(() => {
-    setData(getUseFieldInitial(ids))
-
+    setData(initialValues)
     current_event.current = Emitter.addFieldListener(ids, (payload) => {
+      console.log('asdasdsa', payload)
       setTimeout(() => handleDataSet(payload))
     })
-
     return () => {
       if (current_event.current)
         Emitter.removeFieldListener(current_event.current)
     }
-  }, [JSON.stringify(ids),JSON.stringify(data.id)])
+  }, [JSON.stringify(ids)])
+
   return data
 }
 
